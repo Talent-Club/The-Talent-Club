07de530b84c3c2f62f0ae5cdce5db628bcfe9d75
Casey Ledbetter <caseyledbetter@Caseys-MacBook-Pro.local>
2017-08-23 08:46:34 -0700

    styling of pages, and did multi sn on sign up

--- client/app/signUp/signUp.controller.js
+++ client/app/signUp/signUp.controller.js
@@ -1,31 +1,43 @@
-(function(){
+(function () {
     'use strict';
 
     angular
         .module('app.signUp')
         .controller('SignUpController', SignUpController)
 
-    SignUpController.$inject = ['$stateParams', '$state', 'signUpFactory','authFactory'];
+    SignUpController.$inject = ['$stateParams', '$state', 'signUpFactory', 'authFactory'];
 
     function SignUpController($stateParams, $state, signUpFactory, authFactory) {
         /* jshint validthis:true */
         var vm = this;
 
         vm.register = register;
-        
+        vm.registration = {
+            socialNetworks: []
+        };
+
         function register(registration) {
             console.log(registration);
-            authFactory 
+            authFactory
                 .register(registration)
-                .then(function(response) {
+                .then(function (response) {
                     console.log('world');
-        			$state.go('pending');
-        		})
-                .catch(function(error) {
+                    $state.go('pending');
+                })
+                .catch(function (error) {
                     console.log('error');
-        			alert(error.error_description);
-        		});
+                    alert(error.error_description);
+                });
         }
+
+        vm.addSocialNetwork = function (registration, name, url) {
+
+            registration.socialNetworks.push({
+                name: name,
+                url: url
+            });
+        }
+
         
     }
 
